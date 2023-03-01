7607028462c2a6fd1e6c7c46435d82862b9895f4
NE-O <boopathi_s@outlook.in>
2023-03-01 21:45:56 +0530

    ts,e-1,svelte

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
@@ -7,86 +7,53 @@ import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
-import { electron } from "rollup-plugin-electron";
-import { format } from 'path';
-import { dir } from 'console';
+
+
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-	let server;
-
-	function toExit() {
-		if (server) server.kill(0);
-	}
-
-	return {
-		writeBundle() {
-			if (server) return;
-			server = spawn('npm', ['run', 'start', '--', '--dev'], {
-				stdio: ['ignore', 'inherit', 'inherit'],
-				shell: true
-			});
-
-			process.on('SIGTERM', toExit);
-			process.on('exit', toExit);
-		}
-	};
-}
-
 export default {
-	// electron build
-	input: 'electron/main.ts',
-	output: {
-		sourcemap: true,
-		format:'cjs',
-		dir: "electron/dist"
-	},
-	// --------------------
 
 	input: 'src/main.ts',
 	output: {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'build/bundle/bundle.js',
-		format:'cjs'
+		file: 'build/ui/main.js'
 	},
+
+
 	plugins: [
 		svelte({
 			preprocess: sveltePreprocess({ sourceMap: !production }),
 			compilerOptions: {
-				// enable run-time checks when not in production
 				dev: !production
 			}
 		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
-		css({ output: 'bundle.css' }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
+
+		css({ 
+			output: 'style.css' 
+		}),
+
 		resolve({
 			browser: true,
 			dedupe: ['svelte'],
 			exportConditions: ['svelte']
+
 		}),
-		commonjs(),
-		electron({
-				include: "electron/main.ts",
-				outputDir: "electron/dist"
+
+
+		commonjs({
+			output:"main.cjs"
 		}),
-		typescript({
-			sourceMap: !production,
-			inlineSources: !production
-		}),
+
+
+
 
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
-		!production && serve(),
+		// !production && serve(),
 
 		// Watch the `public` directory and refresh the
 		// browser on changes when not in production
